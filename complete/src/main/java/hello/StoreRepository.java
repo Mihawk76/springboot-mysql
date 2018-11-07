5de5b3a5e20fd308c228af85b438ef2a542146ad
DanielPanjaitan <sdanielchp@gmail.com>
2018-11-07 12:51:36 +0000

    Learned where it went wrong

--- complete/src/main/java/hello/StoreRepository.java
+++ complete/src/main/java/hello/StoreRepository.java
@@ -3,6 +3,8 @@ package hello;
 import org.springframework.data.repository.CrudRepository;
 
 import hello.Store;
+import hello.User;
+import hello.UserRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
--- complete/src/main/java/hello/UserRepository.java
+++ complete/src/main/java/hello/UserRepository.java
@@ -3,6 +3,8 @@ package hello;
 import org.springframework.data.repository.CrudRepository;
 
 import hello.User;
+import hello.Store;
+import hello.StoreRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
